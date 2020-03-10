81495132e0749d5064d316579be18f983fe24e34
gleywson Ribeiro <gleywson@gmail.com>
2020-03-10 14:15:43 -0300

    Inserida a exportação para qualquer pesquisa com as variaáveis de empatia

--- src/main/java/br/gleywson/controle/PesquisaController.java
+++ src/main/java/br/gleywson/controle/PesquisaController.java
@@ -6,12 +6,26 @@
 package br.gleywson.controle;
 
 import br.gleywson.jsf.util.JsfUtil;
+import br.gleywson.modelo.Avaliacao;
 import br.gleywson.modelo.Pesquisa;
+import br.gleywson.modelo.Resposta;
+import br.gleywson.modelo.dao.AvaliacaoFacade;
 import br.gleywson.modelo.dao.PesquisaFacade;
+import java.io.IOException;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.Row;
 import org.eclipse.persistence.exceptions.DatabaseException;
 
 /**
@@ -26,6 +40,9 @@ public class PesquisaController {
     private List<Pesquisa> pesquisas;
     @EJB
     private PesquisaFacade pesquisaFacade;
+    @EJB
+    private AvaliacaoFacade dao;
+    
     
     
     public PesquisaController() {
@@ -67,6 +84,101 @@ public class PesquisaController {
         } catch (Exception e) {
             JsfUtil.addErrorMessage("Existem avaliações para esta pesquisa.");
         }
+    }
+    
+    public void exportar() throws IOException {
+        List<Avaliacao> avaliacoes = dao.getAvaliacoesPorPesquisa(pesquisaFacade.find(pesquisa.getId()));
+        HSSFWorkbook workbook = new HSSFWorkbook();
+        HSSFSheet sheet = workbook.createSheet("tabulacao");
+
+        avaliacoes.forEach((avaliacao) -> {
+            avaliacao.getRespostas().sort((p1, p2) -> p1.getPergunta().getDescricao().compareTo(p2.getPergunta().getDescricao()));
+        });
+
+        int line = 1;
+        int coluna = 0;
+
+        //cabecalho
+        Row cabecalho = sheet.createRow(line - 1);
+        for (Resposta resposta : avaliacoes.get(0).getRespostas()) {
+            Cell celula = cabecalho.createCell(coluna++);
+            celula.setCellValue(resposta.getPergunta().getDescricao());
+            CellStyle cellStyle = sheet.getWorkbook().createCellStyle();
+            Font font = sheet.getWorkbook().createFont();
+            font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+
+            cellStyle.setFillForegroundColor(HSSFColor.GREY_40_PERCENT.index);
+            cellStyle.setFont(font);
+            celula.setCellStyle(cellStyle);
+        }
+
+//        Estilo
+        CellStyle cellStyle = sheet.getWorkbook().createCellStyle();
+        Font font = sheet.getWorkbook().createFont();
+        font.setBoldweight(Font.BOLDWEIGHT_BOLD);
+
+        cellStyle.setFillForegroundColor(HSSFColor.GREY_40_PERCENT.index);
+        cellStyle.setFont(font);
+
+        Cell cell = cabecalho.createCell(coluna++);
+        cell.setCellValue("PT");
+        cell.setCellStyle(cellStyle);
+
+        cell = cabecalho.createCell(coluna++);
+        cell.setCellValue("PD");
+        cell.setCellStyle(cellStyle);
+
+        cell = cabecalho.createCell(coluna++);
+        cell.setCellValue("EC");
+        cell.setCellStyle(cellStyle);
+
+        cell = cabecalho.createCell(coluna++);
+        cell.setCellValue("FS");
+        cell.setCellStyle(cellStyle);
+
+        //corpo da tabela
+        for (Avaliacao avaliacao : avaliacoes) {
+            Row linha = sheet.createRow(line++);
+            int col = 0;
+            for (Resposta resposta : avaliacao.getRespostas()) {
+                Cell celula = linha.createCell(col++);
+                celula.setCellValue(resposta.getOpcao().getDescricao());
+            }
+            Cell celula = linha.createCell(col++);
+            celula.setCellValue(avaliacao.getPT());
+
+            celula = linha.createCell(col++);
+            celula.setCellValue(avaliacao.getPD());
+
+            celula = linha.createCell(col++);
+            celula.setCellValue(avaliacao.getEC());
+
+            celula = linha.createCell(col++);
+            celula.setCellValue(avaliacao.getFS());
+
+        }
+
+        int quantidadeColunas = sheet.getRow(0).getPhysicalNumberOfCells();
+        for (int i = 0; i < quantidadeColunas; i++) {
+            sheet.autoSizeColumn(i);
+        }
+
+        HttpServletResponse response = (HttpServletResponse) FacesContext.getCurrentInstance().getExternalContext().getResponse();
+        response.setContentType("application/vnd.ms-excel");
+        response.setHeader("Content-disposition", "attachment; filename=dados.xls");
+
+        try {
+            try (ServletOutputStream out = response.getOutputStream()) {
+                workbook.write(out);
+                out.flush();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        FacesContext faces = FacesContext.getCurrentInstance();
+        faces.responseComplete();
+
     }
     
 }
